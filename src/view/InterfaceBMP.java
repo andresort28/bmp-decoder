bdaf87addb0035fe87cb1baabdd863ea34e15d94
Andres Ortiz <andresortiz28@hotmail.com>
2015-06-01 16:20:09 -0500

    Project structure

---
+++ src/view/InterfaceBMP.java
@@ -0,0 +1,19 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class InterfaceBMP extends JFrame
+{
+}
